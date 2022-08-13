caf42e77c6c411659f7354e7ee06f752f56d53b0
nixius <chipztery2k@hotmail.com>
2022-08-13 23:39:03 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+var intervalId = window.setInterval(function(){
+
+  // grab all articles
+  var articles = document.querySelectorAll("article");
+   articles.forEach(article => {
+     // loop through all spans in articles
+      const spans = article.querySelectorAll("span");
+     
+     // if a span has a 'shit' text remove the article
+      spans.forEach(span => {
+          if(span.innerText == 'Based on your likes')
+          {
+              span.style.color = 'red'
+              article.remove();
+          }
+          else if(span.innerText == 'Viral Tweets')
+          {
+              span.style.color = 'red'
+              article.remove();
+          }
+          else if(span.innerText == 'Popular images')
+          {
+              span.style.color = 'red'
+              article.remove();
+          }
+         // 'see more' can contain topics we have subscribed to, so only remove 'spam' ones (i.e. not subsrcibed)
+         // which will have an 'X' - also code good ones green :)
+          else if(span.innerText == 'See more')
+          {
+              var spanParent = span.parentElement.parentElement;
+
+              if(spanParent.childNodes.length == 2)
+              {
+                  span.style.color = "green";
+              }
+              else
+              {
+                  span.style.color = "red";
+                  article.remove();
+              }
+          }
+      });
+
+   });
+
+
+}, 200);
